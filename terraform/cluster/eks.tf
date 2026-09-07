0a3b138261ef6f5ee7d4c97236f6ec97ac44e502
bigverl <westonmathews@gmail.com>
2026-09-07 16:50:58 -0700

    alb controller and pod identity

--- terraform/cluster/eks.tf
+++ terraform/cluster/eks.tf
@@ -49,3 +49,25 @@ module "eks" {
     Terraform   = "true"
   }
 }
+
+
+module "aws_lb_controller_pod_identity" {
+  source = "terraform-aws-modules/eks-pod-identity/aws"
+  version = "~> 2.0"
+
+  name = "pokefinder-lbc"
+
+  attach_aws_lb_controller_policy = true
+
+  associations = {
+    this = {
+      cluster_name    = module.eks.cluster_name
+      namespace       = "kube-system"
+      service_account = "aws-load-balancer-controller"
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+  }
+}
